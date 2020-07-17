c6b20453857e220dc4278ab5db54ff46e47df738
Aura Kelloniemi <kaura.dev@sange.fi>
2020-07-17 19:51:49 +0300

    Initial stub for the main module

--- src/lovers_base/__init__.py
+++ src/lovers_base/__init__.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
-from pkg_resources import get_distribution, DistributionNotFound
 
-try:
-    # Change here if project is renamed and does not equal the package name
-    dist_name = __name__
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
-    __version__ = 'unknown'
-finally:
-    del get_distribution, DistributionNotFound
+"""Main module of Lovers' Base.
+
+TODO:Fix this docstring.
+"""
+
+# Retrieve version information
+def _get_version() -> None:
+    """Get package version using distutils."""
+
+    global __version__
+
+    from pkg_resources import get_distribution, DistributionNotFound
+    try:
+        __version__ = get_distribution(__name__).version
+    except DistributionNotFound:
+        __version__ = 'unknown'
+    finally:
+        del get_distribution, DistributionNotFound
+
+__version__ = None
+_get_version()
+
